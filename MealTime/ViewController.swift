62dd61af8f5a1b20adaf562e5c31e460c8856b24
deshollow <555demin@gmail.com>
2024-05-29 17:02:55 +0300

    Initial Commit

---
+++ MealTime/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MealTime
+//
+//  Created by Ivan Akulov on 10/02/2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var array = [Date]()
+    
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let date = Date()
+        array.append(date)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "My happy meal time"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        let date = array[indexPath.row]
+        
+        cell!.textLabel!.text = dateFormatter.string(from: date)
+        return cell!
+    }
+}
+
